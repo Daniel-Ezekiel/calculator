cc6e8f986706fefedc472c2ffcbfd64b0c614757
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-08-20 14:15:14 +0100

    feat: calculator functionality setup - part 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,107 @@
 'use strict';
+
+// Query Selectors
+const calculator = document.querySelector('.calculator');
+const screen = document.querySelector('.screen');
+const nums = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const btnDel = document.querySelector('.btn-delete');
+const btnReset = document.querySelector('.btn-reset');
+
+class Calc {
+  firstOperand = '';
+  secondOperand = '';
+  operation = '';
+
+  add() {
+    this.secondOperand = Number(this.firstOperand) + Number(this.secondOperand);
+  }
+
+  subtract() {
+    this.secondOperand = Number(this.firstOperand) - Number(this.secondOperand);
+  }
+
+  multiply() {
+    this.secondOperand = Number(this.firstOperand) * Number(this.secondOperand);
+  }
+
+  divide() {
+    this.secondOperand = Number(this.firstOperand) / Number(this.secondOperand);
+  }
+
+  eval() {
+    switch (this.operation) {
+      case 'plus':
+        this.add();
+        break;
+      case 'minus':
+        this.subtract();
+        break;
+      case 'times':
+        this.multiply();
+        break;
+      case 'divide':
+        this.divide();
+        break;
+    }
+
+    this.firstOperand = '';
+  }
+
+  delete() {
+    this.secondOperand = this.secondOperand.slice(0, -1);
+  }
+
+  reset() {
+    this.firstOperand = '';
+    this.secondOperand = '';
+    this.operator = '';
+  }
+}
+const newCalc = new Calc();
+
+calculator.addEventListener('click', e => {
+  e.preventDefault();
+  const targetEl = e.target;
+
+  if (targetEl.classList.contains('num')) {
+    newCalc.secondOperand += targetEl.textContent.trim();
+  }
+
+  if (
+    targetEl.classList.contains('point') &&
+    !newCalc.secondOperand.includes('.')
+  ) {
+    newCalc.secondOperand += targetEl.textContent.trim();
+  }
+
+  if (targetEl.classList.contains('operation')) {
+    newCalc.firstOperand = newCalc.secondOperand;
+    newCalc.secondOperand = '';
+
+    newCalc.operation = targetEl.dataset.operation;
+  }
+
+  if (targetEl.classList.contains('btn-delete')) {
+    newCalc.delete();
+  }
+
+  if (targetEl.classList.contains('btn-reset')) {
+    newCalc.reset();
+  }
+
+  if (targetEl.classList.contains('evaluate')) {
+    newCalc.firstOperand && newCalc.secondOperand && newCalc.eval();
+  }
+
+  screen.value = newCalc.secondOperand;
+  console.log(newCalc.secondOperand);
+});
+
+// class Calc {
+//   add() {}
+//   subtract() {}
+//   multiply() {}
+//   divide() {}
+//   evaluate() {}
+// }
