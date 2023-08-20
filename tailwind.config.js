0c74612d038c8a417a55de3ed106aa256155ae05
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-08-20 19:55:31 +0100

    style: improved tailwindcss color configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,42 +42,39 @@ module.exports = {
 
     colors: {
       // Backgrounds
-      'main-1': '#3a4764',
-      'main-2': '#e6e6e6',
-      'main-3': '#160628',
+      'theme1-main': '#3a4764',
+      'theme2-main': '#e6e6e6',
+      'theme3-main': '#160628',
 
-      'toggle-1': '#232c43',
-      'toggle-2': '#d1cccc',
-      'toggle-3': '#1d0934',
+      'theme1-toggle': '#232c43',
+      'theme2-toggle': '#d1cccc',
+      'theme3-toggle': '#1d0934',
 
-      'screen-1': '#182034',
-      'screen-2': '#ededed',
-      'screen-3': '#1d0934',
+      'theme1-screen': '#182034',
+      'theme2-screen': '#ededed',
+      'theme3-screen': '#1d0934',
 
       // Keys
-      'key1-bg-1': '#637097',
-      'key1-bg-2': '#377f86',
-      'key1-bg-3': '#58077d',
+      'theme1-keybg-1': '#637097',
+      'theme1-keybg-2': '#d03f2f',
+      'theme1-keybg-3': '#eae3dc',
+      'theme1-keyshadow-1': '#404e72',
+      'theme1-keyshadow-2': '#93261a',
+      'theme1-keyshadow-3': '#b4a597',
 
-      'key1-shadow-1': '#404e72',
-      'key1-shadow-2': '#1b5f65',
-      'key1-shadow-3': '#bc15f4',
+      'theme2-keybg-1': '#377f86',
+      'theme2-keybg-2': '#ca5502',
+      'theme2-keybg-3': '#e5e4e1',
+      'theme2-keyshadow-1': '#1b5f65',
+      'theme2-keyshadow-2': '#893901',
+      'theme2-keyshadow-3': '#a69d91',
 
-      'key2-bg-1': '#d03f2f',
-      'key2-bg-2': '#ca5502',
-      'key2-bg-3': '#00e0d1',
-
-      'key2-shadow-1': '#93261a',
-      'key2-shadow-2': '#893901',
-      'key2-shadow-3': '#6cf9f2',
-
-      'key3-bg-1': '#eae3dc',
-      'key3-bg-2': '#e5e4e1',
-      'key3-bg-3': '#341c4f',
-
-      'key3-shadow-1': '#b4a597',
-      'key3-shadow-2': '#a69d91',
-      'key3-shadow-3': '#871c9c',
+      'theme3-keybg-1': '#58077d',
+      'theme3-keybg-2': '#00e0d1',
+      'theme3-keybg-3': '#341c4f',
+      'theme3-keyshadow-1': '#bc15f4',
+      'theme3-keyshadow-2': '#6cf9f2',
+      'theme3-keyshadow-3': '#871c9c',
 
       // Text
       'grayish-blue': '#444b5a',
